45d5ff01b2515e25bdec90931bba7c2118a88c2c
sagsso <santiagogalvez17@gmail.com>
2020-08-21 23:01:31 -0500

    Paginator works

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProductsService } from 'src/app/services/products.service';
 import { Product } from './product/product.model';
 import { Router } from '@angular/router';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 
 @Component({
@@ -11,19 +13,24 @@ import { Router } from '@angular/router';
 })
 export class ProductsComponent implements OnInit {
 
-  products: any[];
+  products: Product[];
   displayedColumns: any[] = ['name','priceOut', 'inventory'];
+  dataSource;
 
   constructor(
     private productsService: ProductsService,
     private router: Router
     ) { }
 
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
   ngOnInit(): void {
     this.productsService.getProducts().subscribe((products: any[]) => {
       console.log(products);
       this.products = products;
-      
+      this.dataSource = new MatTableDataSource<Product>(this.products);
+      this.dataSource.paginator = this.paginator;
+
     });
     // console.log(this.products);
   }
